771121c7ec52cfcbaf5928db0c7b79021a58f789
tarandeepmatharu <tarandeepmatharu923@gmail.com>
2019-08-11 12:54:31 +0530

    first animation practice done rotating and scaling in Animations.js

--- App.js
+++ App.js
@@ -1,27 +1,25 @@
 import React,{Component} from 'react';
 import {Text,View,StyleSheet} from 'react-native';
-import {createStackNavigator,createAppContainer} from 'react-navigation';
+import {createSwitchNavigator,createAppContainer} from 'react-navigation';
 import FriendRequests from './screens/FriendRequest';
 import Rne from './screens/Rne';
-
+import Animations from './screens/Animations';
 import Firebase from './screens/Firebase';
 import Exp from './screens/Exp';
 import UserProfile from './screens/UserProfile';
 
-const StackNavigator=createStackNavigator({
+const SwitchNavigator=createSwitchNavigator({
   Home:{screen:FriendRequests},
   Rne:{screen:Rne},
   Firebase:{screen:Firebase},
-  User:{screen:UserProfile}
+  User:{screen:UserProfile},
+  Anim:{screen:Animations}
 },{
  
-initialRouteName:'User',
-defaultNavigationOptions:{
-  header:null
-}
+initialRouteName:'Anim'
 });
 
-const AppContainer=createAppContainer(StackNavigator);
+const AppContainer=createAppContainer(SwitchNavigator);
 export default class App extends Component{
 render(){
   return(
--- screens/Animations.js
+++ screens/Animations.js
@@ -1,18 +1,56 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Animated,View, Text,StyleSheet,Easing} from 'react-native';
 
 export default class Animations extends Component {
-  constructor(props) {
+  fadeAnim=new Animated.Value(0);
+    constructor(props) {
     super(props);
+    this.scaleX=new Animated.Value(0);
+    this.scaleY=new Animated.Value(0);
+    this.rotateMeX=new Animated.Value(0);
     this.state = {
+      
     };
   }
 
+  componentDidMount(){
+      
+      Animated.timing(this.fadeAnim,{toValue:1,duration:1000}).start(()=>{
+        Animated.timing(this.scaleY,{
+          toValue:50,
+          duration:2000
+        }).start(()=>{
+          Animated.timing(this.rotateMeX,{
+            toValue:1,
+            duration:1000
+          }).start();
+        });       
+      });
+  
+    }
+
+
+    
   render() {
+  
+    const spin=this.rotateMeX.interpolate({
+      inputRange:[0,1],
+      outputRange:['0deg','180deg']
+    })
     return (
-      <View>
-        <Text> Animations </Text>
-      </View>
+      <Animated.View style={[styles.container,{opacity:this.fadeAnim}]}>
+        <Animated.Text style={{color:"white",fontSize:this.scaleY,transform:[{rotateX:spin}]}}> Animations {console.log(this.rotateMeX)}</Animated.Text>
+      </Animated.View>
     );
   }
 }
+
+const styles=StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center",
+        backgroundColor:"black",
+
+    }
+});
